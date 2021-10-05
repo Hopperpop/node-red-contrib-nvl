5d8de7db7dba0b007ed2881d9560112e2ca83856
Hopperpop <Hopperpop@users.noreply.github.com>
2021-10-05 14:01:05 +0200

    Version 1.0.0

--- README.md
+++ README.md
@@ -1,1 +1,39 @@
 # node-red-contrib-nvl
+
+Codesys network variable list parser for node-red. With these nodes and the standard udp nodes, you can recieve and send data from/to a Codesys plc.
+This is an improved version of https://flows.nodered.org/node/node-red-contrib-netvar. And is build on top of [jisotalo](https://github.com/jisotalo) [iec-61131-3](https://github.com/jisotalo/iec-61131-3) library.
+
+Supported datatypes:
+- STRUCT, UNION, ARRAY
+- ENUM
+- STRING, WSTRING
+- BOOL
+- BYTE, WORD, DWORD
+- USINT, UINT, UDINT
+- SINT, INT, DINT
+- ULINT, LWORD, LINT
+- TIME, TOD, TIME_OF_DAY
+- DT, DATE_AND_TIME, DATE
+- REAL, LREAL
+
+
+![afbeelding](https://user-images.githubusercontent.com/11853634/136016642-57c9fdd0-a048-474c-ad2c-983352c89916.png)
+
+
+# Notice
+This node hasn't been tested througly yet with real hardware. Bugs can still exist. Contributing by providing raw data telegrams send from a Codesys controller with the associated nvl definiton are appreciated for further development.
+
+# License
+Copyright (c) 2021 Hopperpop
+
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
---
+++ src/nvl-recieve.js
@@ -0,0 +1,116 @@
+const helper = require('./nvl-helper.js');
+const ParseNvlDef = helper.ParseNvlDef;
+
+module.exports = function(RED) {
+    function nvlReciever(config) {
+        RED.nodes.createNode(this,config);
+        var node = this;
+        node.datatypes = {};
+        node.listId = config.listId;
+
+        try{
+            node.nvl = ParseNvlDef(config.definition, node);
+        }catch(err){
+            node.error(err);
+            node.nvl = null;
+        }
+        
+
+        node.on('input', function(msg, send, done) {
+            //Backwards compatibility 
+            send = send || function() { node.send.apply(node,arguments) };
+
+            //Collect and check nvl definition and listId
+            let listId = msg.id || node.listId;
+            let nvl = {};
+            if(typeof msg.nvl === 'string' || msg.nvl instanceof String){
+                //Use dynamic nvl
+                try{
+                    nvl = ParseNvlDef(msg.nvl, node);
+                }catch(err){
+                    if (done) {
+                        // Node-RED 1.0 compatible
+                        done(err);
+                    } else {
+                        // Node-RED 0.x compatible
+                        node.error(err, msg);
+                    }
+                    return;
+                }
+            }else{
+                //Use static configured nvl
+                nvl = node.nvl;
+                //Check if definiton is valid
+                if (nvl === null || typeof nvl !=='object'){
+                    node.status({fill:"red",shape:"ring",text:"Invalid NVL"});
+                    if (done) {done();}
+                    return;
+                }
+            }
+
+            //Check if payload is buffer
+            if (!Buffer.isBuffer(msg.payload)){
+                let err = new Error("Payload is not a buffer.")
+                node.status({fill:"red",shape:"dot",text:"Error"});
+                if (done) {
+                    // Node-RED 1.0 compatible
+                    done(err);
+                } else {
+                    // Node-RED 0.x compatible
+                    node.error(err, msg);
+                }
+                return;
+            }
+
+
+            //Check matching list id
+            if (msg.payload.readUInt16LE(8) != listId){
+                node.status({fill:"yellow",shape:"dot",text:"Wrong list Id"});
+                if (done){done()};
+                return;
+            }else{
+                node.status({fill:"green",shape:"dot",text:""});
+            }
+
+            //Check if size matches
+            let dataSize = msg.payload.length - 20;
+            if(dataSize !== nvl.byteLength){
+                let err = new Error(`Data size doesn\' match. Expected: ${nvl.byteLength} Got: ${dataSize}`);
+                node.status({fill:"red",shape:"dot",text:"Error"});
+                if (done) {
+                    // Node-RED 1.0 compatible
+                    done(err);
+                } else {
+                    // Node-RED 0.x compatible
+                    node.error(err, msg);
+                }
+                return;
+            }
+
+            //Try to parse buffer
+            try{
+
+                msg.payload = nvl.convertFromBuffer(msg.payload.slice(20));
+                send(msg);
+                if (done) {
+                    done();
+                }
+
+            }catch(err){
+
+                //Error during parsing
+                node.status({fill:"red",shape:"dot",text:"Error"});
+                if (done) {
+                    // Node-RED 1.0 compatible
+                    done(err);
+                } else {
+                    // Node-RED 0.x compatible
+                    node.error(err, msg);
+                }
+                return;
+            }
+
+        }); 
+    }
+    RED.nodes.registerType("nvl-recieve",nvlReciever);
+}
